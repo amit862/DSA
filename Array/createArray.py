977de2b062cda3b6891f9f306a007a19cac4a3a5
Amit Verma <v99amit@gmail.com>
2024-04-09 23:37:30 +0530

    Commit

---
+++ Array/createArray.py
@@ -0,0 +1,18 @@
+########  We can create an arrayin python by using:-   (1) Array Module (2) Numpy
+
+# Array Module
+import array
+
+my_array = array.array('i')
+print(my_array)
+my_arr1 = array.array('i',[1,2,3,4])
+print(my_arr1)
+
+
+# Numpy
+
+import numpy as np 
+np_array = np.array([], dtype = int)
+print(np.array)
+np_array1 = np.array([1,2,3,4])
+print(np.array)
