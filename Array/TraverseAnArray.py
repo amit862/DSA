b46b99b78550678cd30a59a52c552fec87c3791a
Amit Verma <v99amit@gmail.com>
2024-04-11 22:13:27 +0530

    Commit

---
+++ Array/TraverseAnArray.py
@@ -0,0 +1,13 @@
+from array import*
+
+arr1 = array('i',[1,2,3,4,5,6])
+arr2 = array('i',[1,3,1,5,1,6])
+
+def traverseArray(array):
+    for i in array:
+        print(i)
+
+# traverseArray(arr1)
+traverseArray(arr2)
+
+
