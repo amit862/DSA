a3452cd79b9815c9b3705a11abf58a47a00704a9
Amit Verma <v99amit@gmail.com>
2024-04-14 21:41:34 +0530

    Commit

---
+++ Array/DeletingAnElement.py
@@ -0,0 +1,13 @@
+########   Deleting an element of an array
+
+from array import*
+
+arr1 = array('i',[1,2,3,4,5])
+
+arr1.remove(4)
+
+print(arr1)
+
+
+####### T.C------> O(n)
+####### S.C---------> O(1)
