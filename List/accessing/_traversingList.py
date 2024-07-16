1f74fdd4cdb352d55fd7ca9c55cbc4aadb9d9981
Amit Verma <v99amit@gmail.com>
2024-07-17 00:35:11 +0530

    Commit

---
+++ List/accessing/_traversingList.py
@@ -0,0 +1,9 @@
+#Created by Amit Verma on 16/07/24
+#Copyright © 2024 CodeWithAmit. All rights reserved.
+
+
+shoppingList = ['Milk', 'Cheese', 'Butter']
+
+for i in range(len(shoppingList)):
+    shoppingList[i] = shoppingList[i]+"+"
+    print(shoppingList[i])
